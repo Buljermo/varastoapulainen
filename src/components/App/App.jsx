08e6e4dfd6c747e9ed9ad513315460ca2695c1f8
DMITRII POPOV <bulpopov@gmail.com>
2023-10-28 21:33:57 +0300

    lisää ButtonContainer-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,22 +4,25 @@ import Header from '../Header/Header'
 import Menu from '../Menu'
 import Content from '../Content'
 import { FloatingButton } from '../../shared/buttons'
+import { ButtonContainer } from '../../shared/buttons'
 
 function App() {
 
   return (
     <>
-      <div className={styles.app}>
-        <Header />
-        <Content>
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <FloatingButton primary>+</FloatingButton>
-        </Content>
-        <Menu />
-      </div>
+      <ButtonContainer>
+        <div className={styles.app}>
+          <Header />
+          <Content>
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <FloatingButton primary>+</FloatingButton>
+          </Content>
+          <Menu />
+        </div>
+      </ButtonContainer>
     </>
   )
 }
