7e56b1c370844f9ab6e77d3a09e9720dbead600a
Dmitrii Popov <bulpopov@gmail.com>
2023-11-01 11:21:45 +0200

    lisää sovelluksen testidatan

---
+++ src/components/App/testdata.js
@@ -0,0 +1,34 @@
+const testdata = [
+    {
+        Id: "1",
+        model: "Samsung RS27T5200SR",
+        type: "Jääkaappi",
+        note: "Tilava, mutta joskus kompressorin ääni voi olla voimakas."
+    },
+    {
+        Id: "2",
+        model: "Bosch Serie 4 HBA534BS0",
+        type: "Uuni",
+        note: "Tasainen lämpötila, helppo puhdistaa."
+    },
+    {
+        Id: "3",
+        model: "Electrolux EWF1476GDW",
+        type: "Pesukone",
+        note: "Hiljainen toiminta, energiatehokas."
+    },
+    {
+        Id: "4",
+        model: "Siemens GS58NAW41",
+        type: "Mikroaaltouuni",
+        note: "Nopeat sulatus- ja lämmitystoiminnot."
+    },
+    {
+        Id: "5",
+        model: "AEG A72700GNX0",
+        type: "Pakastin",
+        note: "Pitää ruoan pitkään tuoreena, hyvä tilavuus."
+    }
+]
+
+export default testdata
