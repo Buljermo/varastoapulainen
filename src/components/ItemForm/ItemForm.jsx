4f5662720a3a85bce8b1af876912ac587416cad5
DMITRII POPOV <bulpopov@gmail.com>
2023-11-06 11:20:31 +0200

    lisää lomakekentät

--- src/components/ItemForm/ItemForm.jsx
+++ src/components/ItemForm/ItemForm.jsx
@@ -3,9 +3,34 @@ import styles from './ItemForm.module.scss'
 function ItemForm(props) {
 
     return (
-        <div>
-            lomake
-        </div>
+        <form>
+            <div className={styles.itemform}>
+                <div className={styles.itemform_row}>
+                    <div>
+                        <label htmlFor='type'>Tuotetyyppi</label>
+                        <select name="type">
+                            <option>Jääkaappi</option>
+                            <option>Pakastin</option>
+                            <option>Uuni</option>
+                            <option>Pesukone</option>
+                            <option>Mikroaaltouuni</option>
+                        </select>
+                    </div>
+                </div>
+                <div className={styles.itemform_row}>
+                    <div>
+                        <label  htmlFor='model'>Malli</label>
+                        <input type="text" name='model' />
+                    </div>
+                </div>
+                <div className={styles.itemform_row}>
+                    <div>
+                        <label  htmlFor='note'>Lisätietoja</label>
+                        <input type="text" name='note' />
+                    </div>
+                </div>
+            </div>
+        </form>
     )
 
 }
