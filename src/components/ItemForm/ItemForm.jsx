2e6f6fcc167a1782e1fbb51962118c975b7df7bb
Dmitrii Popov <bulpopov@gmail.com>
2023-11-13 10:51:50 +0200

    lisää useFormin lomakekäsittelijäksi

--- src/components/ItemForm/ItemForm.jsx
+++ src/components/ItemForm/ItemForm.jsx
@@ -1,14 +1,34 @@
 import styles from './ItemForm.module.scss'
+import useForm from '../../shared/useform/useform'
+import Button from '../../shared/buttons'
 
 function ItemForm(props) {
 
+    const submit = () => {
+        console.log(values)
+        alert("SUBMIT")
+    }
+
+    const initialState = {
+        model: "",
+        tyoe: "",
+        note: ""
+    }
+
+    const { values, handleChange, handleSubmit } = useForm(submit, initialState, false)
+
+    const handleCancel = () => {
+        alert('CANCEL')
+    }
+
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className={styles.itemform}>
                 <div className={styles.itemform_row}>
                     <div>
                         <label htmlFor='type'>Tuotetyyppi</label>
-                        <select name="type">
+                        <select name="type" onChange={handleChange} value={values.type}>
                             <option>Jääkaappi</option>
                             <option>Pakastin</option>
                             <option>Uuni</option>
@@ -19,14 +39,22 @@ function ItemForm(props) {
                 </div>
                 <div className={styles.itemform_row}>
                     <div>
-                        <label  htmlFor='model'>Malli</label>
-                        <input type="text" name='model' />
+                        <label htmlFor='model'>Malli</label>
+                        <input type="text" name='model' onChange={handleChange} value={values.model} />
                     </div>
                 </div>
                 <div className={styles.itemform_row}>
                     <div>
-                        <label  htmlFor='note'>Lisätietoja</label>
-                        <input type="text" name='note' />
+                        <label htmlFor='note'>Lisätietoja</label>
+                        <input type="text" name='note' onChange={handleChange} value={values.note} />
+                    </div>
+                </div>
+                <div className={styles.itemform_row}>
+                    <div>
+                        <Button onClick={handleCancel}>PERUUTA</Button>
+                    </div>
+                    <div>
+                        <Button primary type='submit'>LISÄÄ</Button>
                     </div>
                 </div>
             </div>
