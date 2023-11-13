8fa7284b10aa5d3a803e8ee06e47c7d6374b7fdc
DMITRII POPOV <bulpopov@gmail.com>
2023-11-13 14:06:41 +0200

    lisää lisäyslomakkeen peruutuksen

--- src/components/ItemForm/ItemForm.jsx
+++ src/components/ItemForm/ItemForm.jsx
@@ -1,8 +1,10 @@
 import styles from './ItemForm.module.scss'
 import useForm from '../../shared/useform/useform'
 import Button from '../../shared/buttons'
+import { useNavigate } from 'react-router-dom'
 
 function ItemForm(props) {
+    const navigate = useNavigate()
 
     const submit = () => {
         console.log(values)
@@ -18,7 +20,7 @@ function ItemForm(props) {
     const { values, handleChange, handleSubmit } = useForm(submit, initialState, false)
 
     const handleCancel = () => {
-        alert('CANCEL')
+        navigate('/')
     }
 
 
